c1c6d95f9142f11e8fb1fe52498401c98a55a975
Grace Tan <gracetan37@gmail.com>
2019-01-27 07:57:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,10 @@ function addElementToBeginningOfArray(array,element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+}
