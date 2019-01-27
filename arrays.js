07e182a16f26ba8fb8f06a6b8e65da1784f43c1a
Grace Tan <gracetan37@gmail.com>
2019-01-27 09:06:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array,element) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  array.slice(-2);
   return array
 }
 function destructivelyRemoveElementFromEndOfArray (array){
