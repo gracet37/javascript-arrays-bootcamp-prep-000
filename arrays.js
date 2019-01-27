abcec385ee6860523e837fd6ea6fdb1f7bdabbc5
Grace Tan <gracetan37@gmail.com>
2019-01-27 07:21:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array,element) {
-  var = array;
+  var array = array
   array = ["element",...array]
 }
   function destructivelyAddElementToBeginningOfArray(array,element) {
