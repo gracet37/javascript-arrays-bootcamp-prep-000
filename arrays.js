4374a1a438381792b666bf4a32dfcc2713e6dd8b
Grace Tan <gracetan37@gmail.com>
2019-01-27 07:48:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-//var candyString = "foo"
-function addElementToBeginningOfArray(array,element) {
- return [element,...array]
-}
+
   function destructivelyAddElementToBeginningOfArray(array,element) {
     return `${array}.${element}`
   }
-function addElementToEndOfArray(array,element) {
- return [...array, element]
-}
+
 function destructivelyAddElementToEndOfArray(array,element) {
   return `${array}.${element}`
-}
-function accessElementinArray(array,index) {
-  return `${array}[${index}]`
 }
